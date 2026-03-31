262d5bb76a41a685d8cf38c032dba88122e9d3a6
Vikas Gujjar <www.vgujjar1234@gmail.com>
2026-03-31 21:00:58 +0530

    final update

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: "export",
+  trailingSlash: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-    ],
+    unoptimized: true,
   },
 }
 
